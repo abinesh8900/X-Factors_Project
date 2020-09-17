9e9b86b7bc5bb671925cfb062424753fb32c17b5
pavinane <pavimegan77@gmail.com>
2020-09-17 20:54:48 +0530

    collapse button create

--- landing1.js
+++ landing1.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
     $('.collapse').collapse()
   });
-
-// $(document).ready(function(){
-//     $(".btn-primary").click(function(){
-//       $(".collapse").collapse('toggle');
-//     });
-
-    
-//     $(".btn-success").click(function(){
-//       $(".collapse1").collapse('toggle');
-//     });
-  
-// });
